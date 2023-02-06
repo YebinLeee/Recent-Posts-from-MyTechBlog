f0f1fe62d3783a6bd91a8453bba657f7e47e4aa5
yebinleee <lyb2325@gmail.com>
2023-02-06 12:21:55 +0900

    refactor: file changed

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ for title, link, protected in zip(post_titles, post_links, post_protected):
 
 access_token = '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'
 
-def encode_contents(post_list):
-    file_contents = "# 개인 기술 블로그의 최신 글 모아보기 <br> \n ## Tech \n - `Github's REST API`, [Reference](https://docs.github.com/ko/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents) \n - Web Scraping using `beautifulsoup4` \n - `base64 encoding` \n - Automation using `Github Actions Workflows` \n\n ## Recent Blog Posts \n\n"
-   
+file_contents = "# 개인 기술 블로그의 최신 글 모아보기 <br> \n ## Tech \n - `Github's REST API`, [Reference](https://docs.github.com/ko/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents) \n - Web Scraping using `beautifulsoup4` \n - `base64 encoding` \n - Automation using `Github Actions Workflows` \n\n ## Recent Blog Posts \n\n"
+
+def encode_contents(post_list):   
     for post in post_list:
         if '보호되어 있는 글입니다.' not in str(post['protected']):
             # print(post['title'] + "\n" + post['link'] + "\n" + post['protected'])
@@ -90,9 +90,8 @@ def get_file_sha():
     return file_sha
 
 
-
 def put_new_contents():
-    file_contents = encode_contents(post_list)
+    contents = encode_contents(post_list)
     
     # The commit message
     commit_message = str_now + " : Update README - new tistory blog posts"
@@ -107,7 +106,7 @@ def put_new_contents():
     # The payload for the API request
     payload = {
         "message": commit_message,
-        "content": file_contents
+        "content": contents
     }
 
     # Make the API request to update the file
@@ -122,10 +121,10 @@ def put_new_contents():
     
 
 def patch_new_contents():
-    file_contents = encode_contents(post_list)
+    contents = encode_contents(post_list)
     
     with open("output.txt", "w") as file:
-        file.write(data)
+        file.write(contents)
     
 
     # The commit message
@@ -143,7 +142,7 @@ def patch_new_contents():
     # The payload for the API request
     payload = {
         "message": commit_message,
-        "content": file_contents,
+        "content": contents,
         "sha": file_sha
     }
 
