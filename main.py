5449080707e8314abdeef39685edc75c552234c8
yebinleee <lyb2325@gmail.com>
2023-02-06 12:32:47 +0900

    add: env

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ for title, link, protected in zip(post_titles, post_links, post_protected):
 #    access_token = file.read().strip()
 #    print(access_token)    
 
-access_token = ''
+access_token =  '${{ env.ACCESS_TOKEN}}'
 
 file_contents = "# 개인 기술 블로그의 최신 글 모아보기 <br> \n ## Tech \n - `Github's REST API`, [Reference](https://docs.github.com/ko/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents) \n - Web Scraping using `beautifulsoup4` \n - `base64 encoding` \n - Automation using `Github Actions Workflows` \n\n ## Recent Blog Posts \n\n"
 
