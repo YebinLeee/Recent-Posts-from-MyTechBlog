60a527fabab32d623e724c2e9254eac40a245c3a
yebinleee <lyb2325@gmail.com>
2023-02-06 10:27:36 +0900

    token changed

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ for title, link, protected in zip(post_titles, post_links, post_protected):
 #    access_token = file.read().strip()
 #    print(access_token)    
 
-access_token = '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'
+access_token = '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'
 
 def encode_contents(post_list):
     file_contents = "# 개인 기술 블로그의 최신 글 모아보기 <br> \n ## Tech \n - `Github's REST API`, [Reference](https://docs.github.com/ko/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents) \n - Web Scraping using `beautifulsoup4` \n - `base64 encoding` \n - Automation using `Github Actions Workflows` \n\n ## Recent Blog Posts \n\n"
